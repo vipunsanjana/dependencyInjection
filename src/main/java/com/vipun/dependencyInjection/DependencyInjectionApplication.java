c7e94362ae4a2f4b8557c5612e7c27a036b8ada7
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2024-04-02 13:46:58 +0530

    dependencyInjectionDynamiclyCreate Application context using

--- src/main/java/com/vipun/dependencyInjection/DependencyInjectionApplication.java
+++ src/main/java/com/vipun/dependencyInjection/DependencyInjectionApplication.java
@@ -3,6 +3,8 @@ package com.vipun.dependencyInjection;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class
@@ -12,6 +14,8 @@ public class
 DependencyInjectionApplication {
 
 	public static void main(String[] args) {
+
+		/*
 		// Create a bean factory
 		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
 
@@ -20,6 +24,9 @@ DependencyInjectionApplication {
 
 		// Load the Spring XML configuration file
 		reader.loadBeanDefinitions(new FileSystemResource("configBean.xml"));
+		 */
+
+		ApplicationContext factory = new ClassPathXmlApplicationContext("configBean.xml");
 
 		// Retrieve the Car bean from the factory
 		Car car = (Car) factory.getBean("car");
