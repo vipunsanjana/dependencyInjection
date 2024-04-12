a013810a13613a7e4d227cc1a6833045e58f649d
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2024-04-13 00:43:23 +0530

    setter injection part  autowire part new

--- src/main/java/com/vipun/dependencyInjection/Car.java
+++ src/main/java/com/vipun/dependencyInjection/Car.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Component;
 public class Car {
 
     private int price;
-    private V8 v8engine;
+    private Engine v8engine;
 
 //    public Car(int price, V8 v8engine) {
 //        this.price = price;
 //        this.v8engine = v8engine;
 //    }
 
-    public V8 getV8engine() {
-        return v8engine;
-    }
+//    public V8 getV8engine() {
+//        return v8engine;
+//    }
 
     public void setV8engine(V8 v8engine) {
         System.out.println("Setting v8");
